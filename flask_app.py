841e0f74515922194a4290e279292227c2ff800e
Vivian <v.liu@berkeley.edu>
2023-06-16 16:18:11 -0700

    testing with audio flag on and codec specific to mp3

--- flask_app.py
+++ flask_app.py
@@ -669,7 +669,7 @@ def stitch_videos():
     video_clips = [VideoFileClip(path.replace("%3E",">")) for path in relative_paths]
     print(video_clips)
     final = concatenate_videoclips(video_clips)
-    final.write_videofile("./static/output/stitched_output.mp4", audio=True, temp_audiofile='./static/output/temp-audio.m4a', 
+    final.write_videofile("./static/output/stitched_output.mp4", audio=True, temp_audiofile='./static/output/temp-audio.mp3', audio_codec = 'libmp3lame', 
   remove_temp=False)
     time.sleep(10)
     return "completed"
