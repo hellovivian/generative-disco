219a705316171141ea0a50bc647a4cc526aec660
Vivian <v.liu@berkeley.edu>
2023-06-16 15:44:09 -0700

    testing with audio flag off

--- flask_app.py
+++ flask_app.py
@@ -669,7 +669,7 @@ def stitch_videos():
     video_clips = [VideoFileClip(path.replace("%3E",">")) for path in relative_paths]
     print(video_clips)
     final = concatenate_videoclips(video_clips)
-    final.write_videofile("./static/output/stitched_output.mp4")
+    final.write_videofile("./static/output/stitched_output.mp4", audio=False)
     time.sleep(10)
     return "completed"
 
