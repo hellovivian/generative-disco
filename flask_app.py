d16407718d1a516cfa14e396a7106dc24c04797d
hellovivian <v.liu@berkeley.edu>
2024-02-15 07:50:48 -0800

    Updated docstrings on flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -46,8 +46,24 @@ pipeline = StableDiffusionWalkPipeline.from_pretrained(
 # Background music for videos
 music = 'clairdelune.wav'
 
+
 @app.route('/generate_brainstorm_img', methods=["POST"])
 def generate_brainstorm_img():
+    """
+    Generates an image to help interactively preview the generated video. Returns variations with simple logic (shuffling of prompt by comma-delimited phrases). 
+    To-do: change the simple logic to a GPT-chain.
+
+    Parameters (of the payload):
+    - Prompt
+    - Seed
+
+    Returns:
+    - Does not return a payload. Generates an image in the backend, saves it
+
+    Example:
+    POST /generate_brainstorm_img
+    
+    """
     global num_generations
 
     prompt = request.json["start_prompt"]
@@ -87,6 +103,15 @@ def generate_brainstorm_img():
     return ""
 
 def generate_and_save_image(prompt, curr_seed, filename):
+    """
+    Function that calls the generation pipeline.
+
+    Parameters:
+    - Prompt
+    - Seed
+    - Filename to save as (concatenation of prompt and seed)
+    
+    """
     global num_generations
 
     print(f"GENERATING {prompt} with seed {curr_seed}")
@@ -100,6 +125,14 @@ def generate_and_save_image(prompt, curr_seed, filename):
 
 @app.route('/get_variation', methods=["POST"])
 def get_variation():
+    """
+    GPT chain to get variations of prompts. Permutes the original prompt as per the GPT prompt below. Then generates the image and saves it.
+    Logic in varyImage(element) on the Javascript side refreshes the history on the frontend.
+
+    Parameters (of the payload):
+    - Prompt
+    - OpenAI Key
+    """
     prompt = request.json["prompt"]
     openai_key = request.json["openai_api_key"]
     client = OpenAI(api_key=openai_key)
@@ -130,6 +163,14 @@ def get_variation():
 
 @app.route('/call_generate', methods=["POST"])
 def call_generate():
+    """
+    Utilized in regenerateFrame (which is deprecated on the UI). Generates an image and also saves a preview.
+    
+    Parameters (of the payload):
+    - JSON data representing the intervals. This saves them in the back end to keep state.
+
+
+    """
     global num_generations
     
     json_data = request.json["json_data"]
@@ -158,6 +199,15 @@ def call_generate():
 
 @app.route("/replace_preview", methods=["POST"])
 def replace_preview():
+    """
+    File is copied over to the previews directory such that we can keep state.
+
+    Parameters (of the payload):
+    - draggedPath (image source that was dragged and dropped)
+    - intervalNum (interval in the waveform)
+    - start_or_end (whether that is a start or end preview)
+    
+    """
     draggedPath = request.json["draggedPath"]
     interval_num = request.json["intervalNum"]
     start_or_end = request.json["start_or_end"]
@@ -171,6 +221,11 @@ def replace_preview():
 # Function saves the regions of the waveform
 @app.route("/save_regions", methods=["POST"])
 def save_regions():
+    """
+    Saves regions into the right local file (./static/interval_data.json)
+    Parameters (of the payload):
+    - json_data (represents intervals)
+    """
 
     json_data = request.json["json_data"]
     intervals = json.loads(json.loads(json_data))
@@ -181,6 +236,20 @@ def save_regions():
     
 @app.route("/generate_interval", methods=["POST"])
 def generate_interval():
+    """
+    Core function, abstracts over Stable Diffusion Walk Pipeline. Reads from the interval_data.json that collects state information.
+    Parses it into interval num, start_seed (seed of start preview image), end_seed (seed of end preview image), start_note (start prompt), end_note (end prompt), and current_interval_start (start time offset in music file) and current_interval_end (ending timestamp in music file)
+    Audioreactive flag toggles the smooth flag in the generate_interval function.
+    fps is set to be 24. 
+    Number of interpolation steps is calculated as a function of interval duration and fps.
+    Batch size can be toggled depending on your compute.
+
+    Parameters (of the payload):
+    - draggedPath (image source that was dragged and dropped)
+    - intervalNum (interval in the waveform)
+    - start_or_end (whether that is a start or end preview)
+    
+    """
     global pipeline
     global num_intervals
     json_data = request.json
@@ -209,7 +278,10 @@ def generate_interval():
     underscored_curr_interval_prompt = curr_interval_prompt.replace(" ","_")
     underscored_ending_prompt = ending_prompt.replace(" ","_")
     output_name = f"{start}_{end}_{underscored_curr_interval_prompt}_{curr_interval_seed}->{underscored_ending_prompt}_{ending_seed}".replace(",","-")
+    
+    # Print for feedback on what interval is being generated
     print(curr_interval_prompt, ending_prompt)
+    # Audioreactive flag logic. This controls the alignment to the music. The difference between the two call is that if the smooth_flag is higher, the waveform driving the interpolation is dampened and the visual interpolation is smoother but less audioreactive.
     if (not audio_reactive): 
         video_path = pipeline.walk(
             prompts= [ curr_interval_prompt +" digital painting, 4k", ending_prompt +" digital painting, 4k"],
@@ -251,6 +323,12 @@ def generate_interval():
 # Deletes track (one generated interval)
 @app.route("/delete_track", methods=["POST"])
 def delete_track():
+    """
+    Deletes an interval.
+    Parameters (of the payload):
+    - path_to_delete (path to delete)
+    
+    """
     path_to_delete = request.json["path_to_delete"]
     regex_pattern = r'(.*)(static.*)\/(.*mp4)'
     relative_path =  "./" + re.match(regex_pattern, path_to_delete).group(2)
@@ -261,12 +339,21 @@ def delete_track():
 # Deletes the video
 @app.route("/delete_stitched_video", methods=["POST"])
 def delete_stitched_video():
+    """
+    Deletes the current video output, which is hardcoded to always be `output/stitched_output.mp4`    
+    """
     os.unlink( "./static/output/stitched_output.mp4")
     return "Deleted stitched video"
 
 # Deletes the file
 @app.route("/delete_file", methods=["POST"])
 def delete_file():
+    """
+    Deletes the image file.
+
+    Parameters (of payload)
+    - path_to_delete - Image path
+    """
     path_to_delete = request.json["path_to_delete"]
     regex_pattern = r'(.*)(static.*)(.*)'
     relative_path =  "./" + re.match(regex_pattern, path_to_delete).group(2)
@@ -277,6 +364,9 @@ def delete_file():
 
 @app.route("/brainstorm", methods=["POST"])
 def brainstorm():
+    """
+    Deprecated way of sourcing style keywords, replaced by GPT prompt rewriting.
+    """
     color_transitions = ["blue hour", "light leak", "dark", "muted", "psychedelic", "anaglyph", "polaroid", "geometric", "bloom pass", "gaussian blur", "sepia","color inversion", "neon","saturated", "desaturated", "warm","cool", "pastel", "cyberpunk","pop art", "vintage","film photography", "glitch","vignette"]
     time_transitions = ["sunset", "sunrise", "mosaic", "kaleidoscope", "storybook illustration", "street photography", "medieval", "cubism", "anime", "pixel art", "digital painting", "collage", "low poly",  "strobe", "retro", "dreamy", "cartoon", "golden hour", "dramatic lighting", "sunny", "cloudy", "timelapse", "light tunnels","bokeh", "soft lighting","cinematic lighting", "lens flare", "slow motion", "painting", "sketch", "photorealism"]
     angle_transitions = ["wide angle", "close-up", "birds eye view","midshot", "aerial view", "fisheye", "vaporwave", "swirl", "oil painting","Surrealism", "DSLR", "sculpture", "magazine cover", "ink wash painting", "pattern", "map", "Fauvism", "double exposure", "front view", "side view", "high angle", "fractal", "Impressionism", "cubism", "abstract", "dutch angle", "straight angle", "low angle", "vortex", "upside down", "side profile", "3D render", "motion blur", "watercolor", "grunge", "moonlight", "tilted frame","isometric","medium long" ]
@@ -293,6 +383,14 @@ def brainstorm():
 
 @app.route("/brainstorm_gpt", methods=["POST"])
 def brainstorm_gpt():
+    """
+    Brainstorming with GPT related subjects for a goal visual that the user wants to express. This goal can be abstract (vibe) or concrete (song names, lyrics, images)
+
+    Parameters (of the payload):
+    - goal: Song name, lyric, or image that the user wishes to express. 
+    - openai_api_key: API key
+    
+    """
     goal = request.json["goal"]
     openai_key = request.json["openai_api_key"]
     client = OpenAI(api_key=openai_key)
@@ -318,7 +416,8 @@ def brainstorm_gpt():
     
     subjects = response.split(",")
     subject_prompts = []
-    
+
+    # GPT will output 10 subjects, pick 5
     for subject in subjects[:5]:
     
         response = client.chat.completions.create(
@@ -342,12 +441,18 @@ def brainstorm_gpt():
         subject_prompts.append(response)
 
     for prompt in subject_prompts:
+        # Generate each subject
         generate_and_save_image(prompt, 16, f"{prompt}_16")
     return { "subjects": subject_prompts
     }
 
 @app.route("/stitch_videos", methods=["POST"])
 def stitch_videos():
+    """
+    Moviepy function that concatenates video in a path. It replaces certain stray characters with regex.
+    Audio on concatenated video file seems to have bugs in that it plays on the browser but not in iMovie or on opening the downloaded file
+    
+    """
     video_paths = request.json["video_paths"].split(',')
     regex_pattern = r'(.*)(static.*)(.*)'
     relative_paths = []
@@ -363,20 +468,41 @@ def stitch_videos():
 
 @app.route("/gather_generations", methods=["GET"])
 def gather_generations():
+    """
+    Gathering generations in the static folder.
+    
+    """
     generations_gallery = os.listdir("./static/generations/")
     return jsonify(generations_gallery)
 
 def generate_images(prompt,seed):
+    """
+    Generates images with a prompt and seed.
+
+    Parameters (of the payload):
+    - Prompt
+    - Seed
+    """
     global pipeline
     torch.cuda.empty_cache()
     img = pipeline(prompt +" digital painting, 4k" ,num_inference_steps =50, height=512, width=512, batch_size=1, num_batches=1,seed=int(seed))
     return img
 
 def folder_files_by_ext(folder_path, extension):
+    """
+    Grabs all the files in a folder with a certain extension. In this case, used for getting all .mp4's
+    """
     result = [y for x in os.walk(folder_path) for y in glob(os.path.join(x[0], extension))]
     return result
 
 def find_approx_create_time(interval_folder_name):
+    """
+    Find approximate create time of each video in a folder.
+    This function is used to sort the history of the generations container.
+    
+    Parameters:
+    - interval_folder_name (folder to walk through)
+    """
     internal_folder = next(os.walk("./static/output/" + interval_folder_name))[0]
     files = next(os.walk(internal_folder))[0]
     interval_video = folder_files_by_ext(files,"*.mp4")
@@ -384,6 +510,11 @@ def find_approx_create_time(interval_folder_name):
 
 @app.route("/register_music_change", methods=["POST"])
 def register_music_change():
+    """
+    music, which is a global variable pointer to the music file is changed in value.
+    Parameters (of the payload):
+    - music_choice (string referring to the audio file path). When we swap the global variable, the music variable, which is what generate_interval passes in as a reference to the audiofile is swapped. 
+    """
     global music
     global video
     music = request.json["music_choice"]
@@ -394,6 +525,14 @@ def register_music_change():
 
 @app.route("/change_audio", methods=["POST"])
 def change_audio():
+    """
+    Alternate change audio_function 
+    To-do: Confirm unused, delete
+    
+    Parameters (of the payload):
+    - audio_file
+    - video_file
+    """
 
     global music
     global video
@@ -409,6 +548,16 @@ def change_audio():
 
 @app.route("/download_audio", methods=["POST"])
 def download_audio():
+    """
+    Download music from soundcloud using youtube-dl
+    Deprecated because of issues with cutting the music (different extensions like .mp3 and .m4a) have different ffmpeg rules, is where I left off
+    
+    Parameters (of the payload):
+    - audio_file (music source on soundcloud)
+    - music_start (start time offset in the music)
+    - music_length (duration this value is an int like 10)
+    
+    """
 
     global music
     global video
@@ -446,11 +595,17 @@ def download_audio():
 
 @app.route("/waveform", methods=["GET", "POST"])
 def waveform():
+    """
+    Renders the waveform.html in static/templates/waveform.html
+    """
     return render_template("waveform.html")
 
 
 @app.route("/", methods=["GET", "POST"])
 def run_user_interface():
+    """
+    Renders the /static/templates/run_user_interface.html
+    """
     global music
  
     frames = OrderedDict()
@@ -501,6 +656,8 @@ def run_user_interface():
 
 
 def run():
-
+    """
+    Runs the python app. 
+    """
     app.run(host='0.0.0.0', threaded=True, debug=True, port = 7860)
 run()
